0bd07757bb34a9194dd0aecc517bd8f7e89bba79
Pranjul <pranjulsaxena50@gmail.com>
2025-03-22 17:01:58 +0530

    update backend url

--- Client/src/Helpers/axiosInstance.js
+++ Client/src/Helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5016/api/v1";
-// const BASE_URL = "http://13.201.150.217:5016/api/v1";
+// const BASE_URL = "http://localhost:5016/api/v1";
+const BASE_URL = "http://13.201.150.217:5016/api/v1";
 // const BASE_URL = "/api/v1";
 
 const axiosInstance = axios.create();
