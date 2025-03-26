0c9f5882d561fc7baf6b6041b4cf334a071164d8
Pranjul <147236945+Pranjul-Saxena@users.noreply.github.com>
2025-03-26 13:18:20 +0530

    Update axiosInstance.js

--- Client/src/Helpers/axiosInstance.js
+++ Client/src/Helpers/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://44.204.147.69:5016/api/v1";
+const BASE_URL = "https://app.medicfusion.shop/api/v1";
 // const BASE_URL = "http://localhost:5016/api/v1";
 const axiosInstance = axios.create();
 
